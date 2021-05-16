cd6aeedac5e6a74d55d8341a0dbd856faeaf328b
Markos Girgis <mg.markosgirgis@gmail.com>
2021-05-16 16:52:19 +0300

    Add keywords input component

--- microservices/frontend/src/Create.js
+++ microservices/frontend/src/Create.js
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { WithContext as ReactTags } from "react-tag-input";
+import TagInput from "./components/TagInput";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const blog = { title, body, author };
-
+    const blog = { title, body };
     setIsPending(true);
 
     fetch("http://localhost:8000/blogs/", {
@@ -28,44 +27,6 @@ const Create = () => {
     });
   };
 
-  const KeyCodes = {
-    comma: 188,
-    enter: 13,
-  };
-  const delimiters = [KeyCodes.comma, KeyCodes.enter];
-  const [tags, setTags] = useState([
-    { id: "Thailand", text: "Thailand" },
-    { id: "India", text: "India" },
-  ]);
-  const [suggestions] = useState([
-    { id: "USA", text: "USA" },
-    { id: "Germany", text: "Germany" },
-    { id: "Austria", text: "Austria" },
-    { id: "Costa Rica", text: "Costa Rica" },
-    { id: "Sri Lanka", text: "Sri Lanka" },
-    { id: "Thailand", text: "Thailand" },
-  ]);
-  function handleDelete(i) {
-    const { tags } = this.state;
-    setTags({
-      tags: tags.filter((tag, index) => index !== i),
-    });
-  }
-
-  function handleAddition(tag) {
-    setTags((prevTags) => [...prevTags, tag]);
-  }
-
-  function handleDrag(tag, currPos, newPos) {
-    const newTags = tags.slice();
-
-    newTags.splice(currPos, 1);
-    newTags.splice(newPos, 0, tag);
-
-    // re-render
-    setTags(newTags);
-  }
-
   return (
     <div className="create">
       <h2 className="p-3 m-3">Make a new question</h2>
@@ -73,34 +34,29 @@ const Create = () => {
         <label>Question title:</label>
         <input
           type="text"
+          className="create-input"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           required
         />
-
         <label>Question body:</label>
         <textarea
           type="text"
+          className="create-textarea"
           value={body}
           onChange={(e) => setBody(e.target.value)}
           required
         />
 
         <label>Keywords:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select>
-        <ReactTags
-          tags={tags}
-          suggestions={suggestions}
-          handleDelete={handleDelete}
-          handleAddition={handleAddition}
-          handleDrag={handleDrag}
-          delimiters={delimiters}
-        />
-        {!isPending && <button>Add question</button>}
-        {isPending && <button disabled>Adding... question</button>}
+        <TagInput />
+
+        {!isPending && <button className="create-button">Add question</button>}
+        {isPending && (
+          <button className="create-button" disabled>
+            Adding... question
+          </button>
+        )}
       </form>
     </div>
   );
---
+++ microservices/frontend/src/components/TagInput.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import ReactTags from "react-tag-autocomplete";
+import countries from "../constants/countries";
+
+const TagInput = () => {
+  const [tags, setTags] = useState([
+    { id: 184, name: "Thailand" },
+    { id: 86, name: "India" },
+  ]);
+  const [suggestions, setSuggestions] = useState(countries);
+
+  console.log(setSuggestions);
+
+  function reactTags() {
+    return React.createRef();
+  }
+
+  function onDelete(i) {
+    const newTags = tags.slice(0);
+    newTags.splice(i, 1);
+    setTags(newTags);
+  }
+  function onAddition(newTag) {
+    const newTags = [].concat(tags, newTag);
+    setTags(newTags);
+  }
+
+  function onValidate(tag) {
+    return /^[a-z]{3,12}$/i.test(tag.name);
+  }
+
+  return (
+    <div>
+      <ReactTags
+        allowNew={true}
+        ref={reactTags}
+        tags={tags}
+        suggestions={suggestions}
+        NoSuggestionsText="No suggestion found"
+        onDelete={onDelete}
+        onAddition={onAddition}
+        onValidate={onValidate}
+      />
+      <p>
+        <small>
+          <em>
+            Tags must be 3-12 characters in length and only contain the letters
+            A-Z
+          </em>
+        </small>
+      </p>
+    </div>
+  );
+};
+
+export default TagInput;
