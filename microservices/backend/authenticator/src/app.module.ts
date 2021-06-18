c77b7abe20fb966e6afc36d0944a96d2d29e2911
Markos Girgis <mg.markosgirgis@gmail.com>
2021-06-18 15:25:10 +0300

    Replace ormconfig.json with dotenv configuration on authenticator

--- microservices/backend/authenticator/src/app.module.ts
+++ microservices/backend/authenticator/src/app.module.ts
@@ -3,12 +3,32 @@ import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'rxjs';
 
 @Module({
   imports: [
     UserModule,
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'), //?.host and ?.port override this
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        logging: true,
+        synchronize: false,
+        migrations: ['dist/migration/*{.ts,.js}'],
+        cli: {
+          migrationsDir: 'migration',
+        },
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
