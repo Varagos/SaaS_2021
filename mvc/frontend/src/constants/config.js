35fbfdef4247f297753ef2bcc8e715d3a121d160
Markos Girgis <mg.markosgirgis@gmail.com>
2021-06-30 00:04:35 +0300

    Change heroku app name

--- mvc/frontend/src/constants/config.js
+++ mvc/frontend/src/constants/config.js
@@ -1,6 +1,6 @@
 const server =
   process.env.NODE_ENV === 'production'
-    ? 'https://askmeanything37-mvc.herokuapp.com'
+    ? 'https://askmeanything37-mvc-backend.herokuapp.com'
     : 'http://localhost:8080';
 
 // eslint-disable-next-line import/prefer-default-export
